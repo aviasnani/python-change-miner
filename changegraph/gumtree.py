bf9b684c965843fa04ee024973ee5c603f04ec17
AviAsnani <aviasnani@MACBOOKAIR-3E78.station>
2026-01-01 22:42:58 +0000

    added typelabels

--- changegraph/gumtree.py
+++ changegraph/gumtree.py
@@ -58,6 +58,8 @@ class GumTree:
         KEYWORD = 'keyword'
         LISTCOMP = 'ListComp'
         DICTCOMP = 'DictComp'
+        GENERATOREXPR = 'GeneratorExp'
+        COMPREHENSION = 'comprehension'
 
     def __init__(self, source_path, data):
         self.node_id_to_node = {}
